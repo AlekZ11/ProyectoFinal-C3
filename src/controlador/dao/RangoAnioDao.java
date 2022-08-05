d41ee2dc659a23b64990195e65ed6a101c4aadd3
PatricioBet <patobetan8@gmail.com>
2022-08-04 20:38:58 -0500

    Create RangoAnioDao.java

---
+++ src/controlador/dao/RangoAnioDao.java
@@ -0,0 +1,28 @@
+package controlador.dao;
+
+import modelo.RangoAnio;
+
+/**
+ *
+ * @author patob
+ */
+public class RangoAnioDao extends AdaptadorDao<RangoAnio>{
+    RangoAnio rangoAnio;
+
+    public RangoAnioDao(RangoAnio rangoAnio) {
+        this.rangoAnio = rangoAnio;
+    }
+
+    public RangoAnio getRangoAnio() {
+        return rangoAnio;
+    }
+
+    public void setRangoValores(RangoAnio rangoAnio) {
+        this.rangoAnio = rangoAnio;
+    }
+    
+    public RangoAnioDao() {
+        super(RangoAnio.class);
+    }
+
+}
