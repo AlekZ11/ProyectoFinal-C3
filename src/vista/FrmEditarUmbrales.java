45e152fe6d519e86be9f701f87714a45276dd988
PatricioBet <patobetan8@gmail.com>
2022-08-08 09:24:50 -0500

    vista

--- src/vista/FrmEditarUmbrales.java
+++ src/vista/FrmEditarUmbrales.java
@@ -42,21 +42,29 @@ public class FrmEditarUmbrales extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        txtfUnidadIzq = new javax.swing.JLabel();
-        txtfUnidadDer = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        jLabel9 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jLabel10 = new javax.swing.JLabel();
+        jTextField5 = new javax.swing.JTextField();
+        jButton4 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
         jLabel5 = new javax.swing.JLabel();
         jComboBox2 = new javax.swing.JComboBox<>();
-        jLabel6 = new javax.swing.JLabel();
-        jComboBox3 = new javax.swing.JComboBox<>();
         jLabel7 = new javax.swing.JLabel();
         jComboBox4 = new javax.swing.JComboBox<>();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList<>();
+        jTextField3 = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
@@ -64,65 +72,86 @@ public class FrmEditarUmbrales extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setText("Editar Umbrales");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(252, 6, 150, 24);
+        jLabel1.setBounds(390, 10, 150, 24);
 
         jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel1.setLayout(null);
+        jPanel1.add(jTextField1);
+        jTextField1.setBounds(350, 170, 103, 32);
 
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jLabel2.setText("<= X <=");
+        jLabel2.setText("<= X <");
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(250, 170, 93, 32);
+        jPanel1.add(jTextField2);
+        jTextField2.setBounds(130, 170, 105, 32);
 
         jLabel3.setText("Umbrales");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(258, 2, 61, 16);
 
-        txtfUnidadIzq.setText("Unidad");
+        jLabel9.setText("Clave valor:");
+        jPanel1.add(jLabel9);
+        jLabel9.setBounds(8, 37, 73, 30);
+        jPanel1.add(jTextField4);
+        jTextField4.setBounds(90, 36, 134, 32);
 
-        txtfUnidadDer.setText("Unidad");
+        jLabel10.setText("Descripción:");
+        jPanel1.add(jLabel10);
+        jLabel10.setBounds(242, 36, 75, 32);
+        jPanel1.add(jTextField5);
+        jTextField5.setBounds(323, 36, 239, 32);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(60, 60, 60)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtfUnidadIzq, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(40, 40, 40)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(39, 39, 39)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtfUnidadDer, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(39, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2)
-                    .addComponent(txtfUnidadIzq)
-                    .addComponent(txtfUnidadDer))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        jButton4.setText("Cancelar");
+        jPanel1.add(jButton4);
+        jButton4.setBounds(20, 220, 105, 32);
+
+        jLabel4.setText("Calificación:");
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(8, 80, 74, 24);
+
+        jPanel1.add(jComboBox1);
+        jComboBox1.setBounds(88, 80, 136, 30);
+
+        jLabel5.setText("Categoría:");
+        jPanel1.add(jLabel5);
+        jLabel5.setBounds(250, 90, 60, 16);
+
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Todos", "Todos excepto motos", "Motos" }));
+        jPanel1.add(jComboBox2);
+        jComboBox2.setBounds(320, 80, 240, 30);
+
+        jLabel7.setText("Año");
+        jPanel1.add(jLabel7);
+        jLabel7.setBounds(30, 130, 30, 16);
+
+        jPanel1.add(jComboBox4);
+        jComboBox4.setBounds(90, 120, 290, 30);
+
+        jButton5.setText("Gestionar años");
+        jPanel1.add(jButton5);
+        jButton5.setBounds(410, 120, 150, 30);
+
+        jButton6.setText("Modificar");
+        jPanel1.add(jButton6);
+        jButton6.setBounds(340, 220, 105, 32);
+
+        jButton7.setText("Añadir");
+        jPanel1.add(jButton7);
+        jButton7.setBounds(460, 220, 105, 32);
 
         getContentPane().add(jPanel1);
-        jPanel1.setBounds(10, 310, 660, 81);
+        jPanel1.setBounds(330, 50, 570, 260);
 
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton1.setText("Cancelar");
         getContentPane().add(jButton1);
-        jButton1.setBounds(420, 410, 114, 36);
+        jButton1.setBounds(640, 570, 114, 36);
 
         jButton2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton2.setText("Guardar");
         getContentPane().add(jButton2);
-        jButton2.setBounds(550, 410, 115, 36);
+        jButton2.setBounds(770, 570, 115, 36);
 
         jButton3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton3.setText("Salir");
@@ -132,50 +161,13 @@ public class FrmEditarUmbrales extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton3);
-        jButton3.setBounds(20, 410, 88, 36);
+        jButton3.setBounds(10, 570, 88, 36);
 
-        jLabel4.setText("Categoría:");
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(10, 50, 62, 25);
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Todos", "Todos excepto motos", "Livianos", "Motos", "Taxis", "Bus interprovincial", "Camión carga liviana", "Camioneta y camión de carga liviana", "Carga pesada" }));
-        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jComboBox1);
-        jComboBox1.setBounds(110, 50, 220, 30);
-
-        jLabel5.setText("Año:");
-        getContentPane().add(jLabel5);
-        jLabel5.setBounds(390, 50, 25, 25);
-
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Todos", "X>=2000", "1990<=X<=1999", "X<=1989", "X<=1999", "Controlados por la CTE" }));
-        getContentPane().add(jComboBox2);
-        jComboBox2.setBounds(430, 50, 240, 30);
-
-        jLabel6.setText("Tipo calificación:");
+        jLabel6.setText("Buscar");
         getContentPane().add(jLabel6);
-        jLabel6.setBounds(10, 100, 98, 16);
+        jLabel6.setBounds(20, 50, 40, 30);
 
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Tipo 0", "Tipo 1", "Tipo 2", "Tipo 3" }));
-        getContentPane().add(jComboBox3);
-        jComboBox3.setBounds(110, 100, 220, 30);
-
-        jLabel7.setText("Tipo umbral:");
-        getContentPane().add(jLabel7);
-        jLabel7.setBounds(350, 100, 70, 16);
-
-        jComboBox4.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Alineación", "Suspensión", "Luces", "Ruido", "Emisiones", "Frenos", "Taximetro", "Neumáticos", "Bus intra e inter: chasis y carrocería", "Transporte de carga", " " }));
-        jComboBox4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox4ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jComboBox4);
-        jComboBox4.setBounds(430, 100, 240, 30);
-
+        jList1.setFont(new java.awt.Font("Segoe UI", 0, 10)); // NOI18N
         jList1.setModel(new javax.swing.AbstractListModel<String>() {
             String[] strings = { "ALINEACIÓN 1ER EJE CONVERGENCIA", "ALINEACIÓN 1ER EJE DIVERGENCIA", "EFICACIA SUSPENSIÓN EN RUEDA DERECHA DEL 1ER EJE", "DESEQUILIBRIO DE SUSPENSIÓN DEL 1ER EJE", "EFICACIA SUSPENSIÓN EN RUEDA DERECHA DEL 2DO EJE", "DESEQUILIBRIO DE SUSPENSIÓN DEL 2DO EJE", "ALINEACIÓN HORIZONTAL FARO CONDUCTOR", "ALINEACIÓN VERTICAL FARO CONDUCTOR" };
             public int getSize() { return strings.length; }
@@ -184,22 +176,21 @@ public class FrmEditarUmbrales extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jList1);
 
         getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(10, 150, 660, 150);
+        jScrollPane1.setBounds(10, 90, 310, 220);
+        getContentPane().add(jTextField3);
+        jTextField3.setBounds(60, 50, 260, 30);
 
-        setBounds(0, 0, 699, 503);
+        jScrollPane2.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane2);
+        jScrollPane2.setBounds(10, 320, 880, 240);
+
+        setBounds(0, 0, 925, 659);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         System.exit(0);
     }//GEN-LAST:event_jButton3ActionPerformed
-
-    private void jComboBox4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox4ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox4ActionPerformed
-
-    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jComboBox1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -240,23 +231,31 @@ public class FrmEditarUmbrales extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
-    private javax.swing.JComboBox<String> jComboBox3;
     private javax.swing.JComboBox<String> jComboBox4;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JList<String> jList1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
-    private javax.swing.JLabel txtfUnidadDer;
-    private javax.swing.JLabel txtfUnidadIzq;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
     // End of variables declaration//GEN-END:variables
 }
